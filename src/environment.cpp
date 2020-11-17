e9a60e29c9290453dd52cc6493b1aabb7e243a0b
Alexandru Bianca <Bianca.Alexandru@elektrobit.com>
2020-11-17 15:25:49 +0200

    Added nuScenes LIDAR data

--- src/environment.cpp
+++ src/environment.cpp
@@ -133,7 +133,7 @@ int main (int argc, char** argv)
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
     ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
-    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
+    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_2");
     auto streamIterator = stream.begin();
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
     while (!viewer->wasStopped ())
